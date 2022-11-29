95a5277ce5439f089963bbe4583e3211f700aa3f
Marcos Paulo <itsolutions031250@gmail.com>
2022-11-29 14:13:47 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using produtosExemploClassMetod;
+using System.Globalization;
+
+Produto p = new Produto();
+
+Console.WriteLine("Entre com os dados do produto:");
+Console.Write("Nome: ");
+p.Nome = Console.ReadLine();
+Console.Write("Preço: ");
+p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Quantidade no estoque: ");
+p.Quantidade = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Dados do produto: ");
